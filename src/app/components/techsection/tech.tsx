3fbb27ca779f9e20ad26b5d04920832117ed2c1a
eksqtr <eksqtr@gmail.com>
2024-08-20 23:18:37 +0800

    Page design adjustments, bug fixes, enhancement.
    - Revamping each sections to component based\n- Enhancement of designs on each sections and navigation bar by adding padding based on dynamically screen sizes of the users.\n- Implementation of one-page scrolling design for simplicity and easy to navigate by snapping on each Y side of sections and it will only works on larger screens.\n- Changing Skills menu to Tech Stacks for some reason it makes sense to me.\n- Bug fixed on colored active menu when toggling dark mode instead of text-gray-300 now it shows as bright yellow

---
+++ src/app/components/techsection/tech.tsx
@@ -0,0 +1,33 @@
+
+
+export const TechSection = () => {
+    return (
+        <section className="md:px-96 sm:px-10 flex justify-center items-center flex-col md:flex-row md:h-screen" id="tech-stack">
+            {/* Left Side */}
+            <div className="p-20 text-center md:text-left">
+                <h2 className="text-5xl py-2 text-teal-600 font-medium">Eubie Jay Clemente</h2>
+                <h3 className="text-2xl py-2 dark:text-gray-300">Software Engineer - Full Stack</h3>
+                <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-400">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a venenatis dolor. 
+                        Aliquam erat volutpat. Proin condimentum tincidunt maximus. Vivamus ut iaculis leo. 
+                        Donec leo sem, feugiat eu eleifend scelerisque, bibendum ac leo. Ut bibendum placerat nibh.
+                </p>
+                        
+            </div>
+
+            {/* Right Side */}
+            <div className="">
+                <div>
+                    <h3 className="text-3xl py-1 dark:text-gray-300">Work with Me</h3>
+                    <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-400">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a venenatis dolor. 
+                            Aliquam erat volutpat. Proin condimentum tincidunt maximus. Vivamus ut iaculis leo. 
+                            Donec leo sem, feugiat eu eleifend scelerisque, bibendum ac leo. Ut bibendum placerat nibh.
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default TechSection;
